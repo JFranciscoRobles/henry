e79bf6e14a603ff2c87fd5a1bdb7c4e25b127b85
JFranciscoRobles <josefrancisco.roblesacosta@gmail.com>
2021-12-09 16:33:31 -0800

    git push

--- pages/api/postAccount.ts
+++ pages/api/postAccount.ts
@@ -1,13 +1,32 @@
 import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
+import _ from "lodash";
 
 export default async function handle(req: any, res: any) {
   const { data, carrito, precio } = req.body.data;
-  const formatCarrito = carrito.map((item: any) => {
-    return { id: item.producto.id };
-  });
   const formatDate = new Date(data.fecha);
+  const formatCarrito = carrito.map((item: any) => {
+    return item.producto;
+  });
 
+  const countCarrito = _(formatCarrito)
+    .groupBy("id")
+    .map(function (items, id) {
+      return {
+        id: parseInt(id),
+        count: items.length,
+        price: items[0].precio,
+      };
+    })
+    .value();
+
+  const newFormatCarrito = countCarrito.map((item: any) => {
+    return {
+      cantidad: item.count,
+      precioTotal: item.price * item.count,
+      objeto: { connect: { id: item.id } },
+    };
+  });
   try {
     await prisma.cuenta.create({
       data: {
@@ -15,7 +34,9 @@ export default async function handle(req: any, res: any) {
         nombreEmpleado: data.nombreEmpleado,
         fecha: formatDate,
         precioFinal: precio,
-        objetos: { connect: formatCarrito },
+        objetos: {
+          create: newFormatCarrito,
+        },
       },
     });
     return res.json({ msg: "Cuenta creada" });
